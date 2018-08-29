36332850f8d9f7ee1bfd87d0c1b77e7bc7fcf99e
xwen <wen.xi@husky.neu.edu>
2018-08-29 16:12:43 -0400

    Persons component

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 // Component name should be capitalized because in JSX all elements starting
 // with lowercase characteres are reserved for the native HTML elements.
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
   state = {
@@ -57,14 +57,10 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
         </div>
       );
 
