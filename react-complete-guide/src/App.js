853c15cc4d807ad832f06e9e2e5b6b8910cd4f52
xwen <wen.xi@husky.neu.edu>
2018-08-29 13:29:34 -0400

    dynamic style and className

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -51,7 +51,8 @@ class App extends Component {
   // some calculation, listening to events etc.
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -73,12 +74,22 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
