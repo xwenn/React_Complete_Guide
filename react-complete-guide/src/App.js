cd04711630dd1a4a05241c89d3dd71cabae94c0e
xwen <wen.xi@husky.neu.edu>
2018-08-29 14:16:46 -0400

    using Radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 // Component name should be capitalized because in JSX all elements starting
 // with lowercase characteres are reserved for the native HTML elements.
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -96,14 +96,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Radium from 'radium';
 
 import './Person.css';
 
 // person is a function instead of a class, so we don't import or extend Component
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
@@ -13,4 +19,4 @@ const person = (props) => {
   )
 };
 
-export default person;
+export default Radium(person);
