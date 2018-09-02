1475fdc67cb89cba03de6140ff712af80f4dc528
xwen <wen.xi@husky.neu.edu>
2018-09-01 23:00:42 -0400

    PropType

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
@@ -13,6 +14,13 @@ const Person = (props) => {
       <input type="text" onChange={props.changed} value={props.name} />
     </Aux>
   )
+};
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func 
 };
 
 export default withClass(Person, classes.Person);
