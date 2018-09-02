6ddd3f75e06c5728eebc65f3b9b3ebc80213c030
xwen <wen.xi@husky.neu.edu>
2018-09-02 11:17:32 -0400

    more ref since React16.3

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
-// person is a function instead of a class, so we don't import or extend Component
 class Person extends Component {
   constructor(props) {
     super(props);
     console.log('[Person.js] Inside Constructor', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -19,8 +19,12 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
+  }
+
+  focus() {
+    this.inputElement.current.focus();
   }
 
   render() {
@@ -30,7 +34,7 @@ class Person extends Component {
         <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input
-          ref={(inp) => { this.inputElement = inp }}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name} />
--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-      click={() => props.clicked(index)}
-      name={person.name}
-      position={index}
-      age={person.age}
-      key={person.id}
-      changed={(event) => props.changed(event, person.id)} />
-  });
+class Persons extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('[Persons.js] Inside Constructor', props);
+    this.lastPersonRef = React.createRef();
+  }
 
-export default persons;
+  componentWillMount() {
+    console.log('[Persons.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] Inside componentWillMount()');
+    this.lastPersonRef.current.focus();
+  }
+
+  render() {
+    console.log('[Persons.js] Inside render()');
+    return this.props.persons.map((person, index) => {
+      return <Person
+        click={() => this.props.clicked(index)}
+        name={person.name}
+        position={index}
+        age={person.age}
+        ref={this.lastPersonRef}
+        key={person.id}
+        changed={(event) => this.props.changed(event, person.id)} />
+    });
+  }
+}
+
+export default Persons;
