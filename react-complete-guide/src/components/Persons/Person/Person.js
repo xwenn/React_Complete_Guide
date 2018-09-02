42ac148403464fdd1d2eb35934f16a6cb63bfcce
xwen <wen.xi@husky.neu.edu>
2018-09-02 10:53:04 -0400

    use ref for focusing

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Person.css';
@@ -6,21 +6,44 @@ import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
 // person is a function instead of a class, so we don't import or extend Component
-const Person = (props) => {
-  return (
-    <Aux>
-      <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </Aux>
-  )
-};
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] Inside Constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[Person.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Person.js] Inside componentDidMount()');
+    if (this.props.position === 0) {
+      this.inputElement.focus();
+    }
+  }
+
+  render() {
+    console.log('[Person.js] Inside render()');
+    return (
+      <Aux>
+        <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input
+          ref={(inp) => { this.inputElement = inp }}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name} />
+      </Aux>
+    )
+  }
+}
 
 Person.propTypes = {
   click: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
-  changed: PropTypes.func 
+  changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
